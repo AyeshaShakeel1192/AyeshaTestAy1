93992434ce461f28b2b31c63798a2e56a3ec4f66
Ayesha <ayesha.shakeel9899@gmail.com>
2022-06-16 17:22:25 -0400

    Added new activity, image button, changed the text view color to red.

--- AyeshaTestAA1/src/main/java/ayesha/shakeel/n01451192/a/Ay1Activity1.java
+++ AyeshaTestAA1/src/main/java/ayesha/shakeel/n01451192/a/Ay1Activity1.java
@@ -5,13 +5,30 @@ package ayesha.shakeel.n01451192.a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Ay1Activity1 extends AppCompatActivity {
+
+    //declare the image Button1
+    ImageButton btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (ImageButton) findViewById(R.id.Aye1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ay1Activity1.this);
+            }
+        });
+
     }
+
+
 }
---
+++ AyeshaTestAA1/src/main/java/ayesha/shakeel/n01451192/a/Sh2Acitivity.java
@@ -0,0 +1,14 @@
+package ayesha.shakeel.n01451192.a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sh2Acitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sh2);
+    }
+}
