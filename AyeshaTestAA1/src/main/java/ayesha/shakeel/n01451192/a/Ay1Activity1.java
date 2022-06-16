a776ec600d31b3de7bab9e5e3de5cea9156d18db
Ayesha <ayesha.shakeel9899@gmail.com>
2022-06-16 15:35:42 -0400

    This is my initial commit

---
+++ AyeshaTestAA1/src/main/java/ayesha/shakeel/n01451192/a/Ay1Activity1.java
@@ -0,0 +1,17 @@
+/* Ayesha Shakeel n01451192
+   Midterm test Practise run - CENG258 - SECTION ...
+ */
+package ayesha.shakeel.n01451192.a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ay1Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
